6731aff16fefe7986121bd08011720c13f59889b
Devendra Kushwah <idevendra08@gmail.com>
2019-03-05 19:37:56 +0530

    exception handle

--- lyrics.py
+++ lyrics.py
@@ -8,17 +8,21 @@ lyrics scrapped from : http://www.metrolyrics.com/
 import requests, bs4
 
 def get_url_from_google(song,artist):
-  page=page=requests.get("https://www.google.com/search?&q="+artist+"+"+song+"+lyrics+metrolyrics")
-  soup=bs4.BeautifulSoup(page.text,'html.parser')
-  data=str((soup.find_all('div',{'class':'g'}))[0].find_all('a')[0])
-  http=data.index('http')
-  html=data.index('html')
-
-  return data[http:html+4]
-  
+  try:
+    page=requests.get("https://www.google.com/search?&q="+artist+"+"+song+"+lyrics+metrolyrics")
+    soup=bs4.BeautifulSoup(page.text,'html.parser')
+    data=str(((soup.find_all('div',{'class':'g'}))[0]).find_all('a')[0])
+    http=data.index('http')
+    html=data.index('html')
+    return data[http:html+4]
+  except:
+    return ''
   
 def get_lyrics(song,artist):
-  page=requests.get(get_url_from_google(song,artist))
+  link=get_url_from_google(song,artist)
+  if(link==''):
+    return ''
+  page=requests.get(link)
   soup=bs4.BeautifulSoup(page.text,'html.parser')
   lyrics = soup.find_all('p', {'class':'verse'})
   for i in lyrics:
